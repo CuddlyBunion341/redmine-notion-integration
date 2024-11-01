0c04978dee1ec47477d1091481ae51153d33fafa
Daniel Bengl <daniel.bengl@renuo.ch>
2024-11-01 15:47:20 +0100

    WIP notion integration

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import { config } from 'dotenv';
 
 config();
 
+// Redmine
+
 const redmineApiKey = process.env.REDMINE_API_KEY;
 const redmineUrl = process.env.REDMINE_URL;
 
@@ -13,6 +15,55 @@ async function fetchRedmineTickets() {
   console.log(response.data);
   return response.data.issues.map((issue: any) => ({name: issue.subject, status: issue.status.name}));
 }
+
+
+// notion
+
+const notionDbId = process.env.NOTION_DB_ID;
+const notionSecret = process.env.NOTION_SECRET;
+
+async function logNotionDatabaseRecords() {
+  const url = `https://api.notion.com/v1/databases/${notionDbId}/query`
+  console.log(url)
+  await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${notionSecret}`,
+      // 'Notion-Version': '2021-05-13',
+      "Notion-Version": "2022-06-28",
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+console.log(logNotionDatabaseRecords());
+
+type RedmineTicket = {
+  name: string;
+  status: string;
+}
+
+async function createNotionEntry(ticket: RedmineTicket) {
+  const payload = {
+    json: {
+      type: 'page',
+      properties: {
+        Title: [{ type: 'title', id: 'title' }],
+        // Add other properties as needed
+      },
+    },
+  };
+
+  const response = await axios.post(`https://api.notion.com/v1/databases/${notionDbId}/pages`, payload, {
+    headers: {
+    Authorization: `Bearer ${notionSecret}`,
+    'Notion-Version': '2021-05-13',
+  },
+  });
+
+  return response.data;
+}
+
+// Server
 
 serve({
   async fetch(req) {
