e5e68413b214c327cfe481ba3e65f3b978ab4d28
Daniel Bengl <daniel.bengl@renuo.ch>
2024-11-14 15:02:29 +0100

    Wip

--- index.ts
+++ index.ts
@@ -2,7 +2,14 @@ import { serve } from 'bun';
 import axios from 'axios';
 import { config } from 'dotenv';
 import { fetchRedmineIssues } from './src/redmine';
+import { hasColumn, createColumn, fetchColumnOptions, updateColumnOptions } from './src/notion';
 
 config();
 
-console.log(await fetchRedmineIssues());
+const columnName = process.env.NOTION_REDMINE_TICKET_COLUMN_NAME;
+
+if (!hasColumn(columnName)) {
+  await createColumn(columnName, []);
+}
+
+console.log(await fetchColumnOptions(columnName));
--- src/notion.ts
+++ src/notion.ts
@@ -3,3 +3,52 @@ import { fetchRedmineTickets} from "./redmine";
 
 const notion = new Client({ auth: process.env.NOTION_SECRET });
 const notionDbId = process.env.NOTION_DB_ID;
+
+type NotionSelectOption = {
+  name: string;
+  color: string;
+}
+
+export function hasColumn(columnName: string) {
+  return notion.databases.retrieve({ database_id: notionDbId })
+  .then(response => {
+    return columnName in response.properties;
+  });
+}
+
+export function createColumn(columnName: string, options: NotionSelectOption[] = []) {
+  return notion.databases.update({
+    database_id: notionDbId,
+    properties: {
+      [columnName]: {
+        multi_select: {
+          options
+        }
+      }
+    }
+  });
+}
+
+export function fetchColumnOptions(columnName: string) {
+  return notion.databases.retrieve({ database_id: notionDbId })
+  .then(response => {
+    console.log(Object.keys(response.properties))
+    console.log(columnName)
+    const property = response.properties[columnName];
+    console.log(property)
+    return property.multi_select.options as NotionSelectOption[];
+  });
+}
+
+export function updateColumnOptions(columnName: string) {
+  return notion.databases.update({
+    database_id: notionDbId,
+    properties: {
+      [columnName]: {
+        multi_select: {
+          options: fetchColumnOptions(columnName)
+        }
+      }
+    }
+  });
+}
